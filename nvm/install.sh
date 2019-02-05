f9949fd904811ca6d8f0eac1935c43a6b8d3662d
KJaddoe <KJaddoe>
2019-02-05 14:40:07 +0100

    install nvm

--- nvm/install.sh
+++ nvm/install.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-if command -v nvm >/dev/null 2>&1; then
-  wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.00/install.sh | zsh
-fi
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.00/install.sh
