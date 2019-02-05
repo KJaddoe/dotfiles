688e09d689da2f767ae0fbd5f98b47a33f74c115
KJaddoe <KJaddoe>
2019-02-05 15:01:35 +0100

    docker

--- docker/install.sh
+++ docker/install.sh
@@ -4,6 +4,8 @@ mkdir -p "$HOME/.docker/completions"
 if command -v docker-compose >/dev/null 2>&1; then
 	curl -sL https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose \
 		-o "$HOME/.docker/completions/_docker-compose"
+  sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
 fi
 if command -v docker-machine >/dev/null 2>&1; then
 	curl -sL https://raw.githubusercontent.com/docker/machine/master/contrib/completion/zsh/_docker-machine \
@@ -12,6 +14,10 @@ fi
 if command -v docker >/dev/null 2>&1; then
 	curl -sL https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker \
 		-o "$HOME/.docker/completions/_docker"
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+  sudo apt install docker-ce -y
+  sudo usermod -aG docker ${USER}
+  su - ${USER}
 fi
 
-sudo apt install docker-ce -y
--- nvm/install.sh
+++ nvm/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 if command -v nvm >/dev/null 2>&1; then
-  wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.00/install.sh | zsh
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh
 fi
