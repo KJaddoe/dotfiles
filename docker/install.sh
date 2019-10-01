43d78cc4ce5ff5e002aec74af7328016c6f3e3d2
KJaddoe <KJaddoe>
2019-10-01 13:13:07 +0200

    remove docker, docker-compose and docker-machine installs
    these are handled in machine

--- docker/install.sh
+++ docker/install.sh
@@ -1,22 +1,2 @@
 #!/bin/sh
 mkdir -p "$HOME/.docker/completions"
-
-if command -v docker-compose >/dev/null 2>&1; then
-	curl -sL https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose \
-		-o "$HOME/.docker/completions/_docker-compose"
-  sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
-fi
-if command -v docker-machine >/dev/null 2>&1; then
-	curl -sL https://raw.githubusercontent.com/docker/machine/master/contrib/completion/zsh/_docker-machine \
-		-o "$HOME/.docker/completions/_docker-machine"
-fi
-if command -v docker >/dev/null 2>&1; then
-	curl -sL https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker \
-		-o "$HOME/.docker/completions/_docker"
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-  sudo apt install docker-ce -y
-  sudo usermod -aG docker ${USER}
-  su - ${USER}
-fi
