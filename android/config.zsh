c8c822be48643641a927df6ebd7227f5029baf75
KJaddoe <k.a.jaddoe@gmail.com>
2020-10-06 12:36:29 +0000

    Update config.zsh

--- android/config.zsh
+++ android/config.zsh
@@ -4,4 +4,4 @@ export ANDROID_HOME=~/Android
 export PATH=$ANDROID_HOME/tools:$PATH
 export PATH=$ANDROID_HOME/platform-tools:$PATH
 
-export ADB_SERVER_SOCKET=tcp:$WSL_HOST:5037
+export ADB_SERVER_SOCKET=tcp:0.0.0.0:5037
