b5e3d64cc99dbfa33f6c2c08e181b5c757beca5f
Karan Jaddoe <k.a.jaddoe@gmail.com>
2021-05-03 19:20:12 +0200

    use the WSL_HOST to connect to the windows adb server
    in order for this to work you need to run the adb server from windows
    using something like powershell

--- android/config.zsh
+++ android/config.zsh
@@ -4,4 +4,5 @@ export ANDROID_HOME=/opt/android-sdk-linux
 export PATH=$ANDROID_HOME/tools:$PATH
 export PATH=$ANDROID_HOME/platform-tools:$PATH
 
-export ADB_SERVER_SOCKET=tcp:0.0.0.0:5037
+export WSL_HOST=$(tail -1 /etc/resolv.conf | cut -d' ' -f2)
+export ADB_SERVER_SOCKET=tcp:$WSL_HOST:5037
--- wsl/config.zsh
+++ wsl/config.zsh
@@ -1,3 +1,2 @@
 #!/bin/zsh
-# export WSL_HOST=$(tail -1 /etc/resolv.conf | cut -d' ' -f2)
-export WSL_HOST='0.0.0.0'
+export WSL_HOST=$(tail -1 /etc/resolv.conf | cut -d' ' -f2)
