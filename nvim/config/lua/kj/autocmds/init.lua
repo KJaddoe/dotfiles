14ebfae02c0e7e67532c6b84e9a0b33244d4c57b
Karan Jaddoe <KJaddoe>
2021-12-09 10:59:14 +0100

    bugfix

--- nvim/config/lua/kj/autocmds/init.lua
+++ nvim/config/lua/kj/autocmds/init.lua
@@ -8,7 +8,7 @@ M.autocmd("BufEnter",     "*",   "if &buftype == 'terminal' | :startinsert | end
 M.autocmd("BufReadPost",  "*",   [[if line("'\"") > 0 && line ("'\"") <= line("$") | exe "normal! g'\"" | endif]])
 M.autocmd("BufWritePre",  "*",   "%s/\\s\\+$//e")
 
-M.autocmd("ColorScheme",  "*",   "lua require('mh.colors').setItalics()")
+M.autocmd("ColorScheme",  "*",   "lua require('kj.colors').setItalics()")
 M.autocmd("CompleteDone", "*",   "pclose")
 
 M.autocmd("FileType",     "vue", "syntax sync fromstart")
