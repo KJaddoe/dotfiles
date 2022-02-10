4404a6a5fa5d2b13cbacbc2a32ccfaff827133e5
Karan Jaddoe <KJaddoe>
2022-02-10 13:31:22 +0100

    Remove the omnisharp lsp, because it is not working

--- nvim/config/lua/kj/lsp/init.lua
+++ nvim/config/lua/kj/lsp/init.lua
@@ -272,11 +272,4 @@ lspconfig.angularls.setup {
   end
 }
 
-lspconfig.omnisharp.setup {
-  on_attach = function(_, bufnr)
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-  end,
-  cmd = { "~/.cache/omnisharp-vim/omnisharp-roslyn/run", "--languageserver" , "--hostPID", tostring(pid) },
-}
-
 return M
