538832f240207e4573e724e73ecb74449f8aa5e5
Karan Jaddoe <k.a.jaddoe@gmail.com>
2022-06-24 15:33:28 +0200

    packer update

--- nvim/config/plugin/packer_compiled.lua
+++ nvim/config/plugin/packer_compiled.lua
@@ -127,6 +127,11 @@ _G.packer_plugins = {
     path = "/home/anirvan/.local/share/nvim/site/pack/packer/start/formatter.nvim",
     url = "https://github.com/mhartington/formatter.nvim"
   },
+  ["friendly-snippets"] = {
+    loaded = true,
+    path = "/home/anirvan/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    url = "https://github.com/rafamadriz/friendly-snippets"
+  },
   ["github-light.vim"] = {
     loaded = true,
     path = "/home/anirvan/.local/share/nvim/site/pack/packer/start/github-light.vim",
@@ -581,5 +586,6 @@ if should_profile then save_profiles() end
 end)
 
 if not no_errors then
+  error_msg = error_msg:gsub('"', '\\"')
   vim.api.nvim_command('echohl ErrorMsg | echom "Error in packer_compiled: '..error_msg..'" | echom "Please check your config for correctness" | echohl None')
 end
