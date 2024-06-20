c153200ce607dff27a51bac018595054ce45a5a4
Karan Jaddoe <k.a.jaddoe@gmail.com>
2024-06-20 14:12:53 +0200

    Set keymap for escape back to jk/kj

--- nvim/config/lua/user/plugins/init.lua
+++ nvim/config/lua/user/plugins/init.lua
@@ -182,7 +182,7 @@ require("lazy").setup({
     "max397574/better-escape.nvim",
     config = function()
       require("better_escape").setup({
-        mapping = { "jj" },
+        mapping = {"jk", "kj"},
         timeout = 100,
       })
     end,
