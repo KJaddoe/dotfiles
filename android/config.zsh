075a3b813a0449e1411e83aa74a65f52691a2d1c
KJ <KJ>
2024-06-25 14:23:38 +0200

    Install android command line tools for linux

--- android/config.zsh
+++ android/config.zsh
@@ -1,7 +1,8 @@
 #!/bin/zsh
-export ANDROID_HOME=/opt/android-sdk-linux
+export ANDROID_HOME=$HOME/Android
+export ANDROID_SDK_ROOT=$ANDROID_HOME
 
-export PATH=$ANDROID_HOME/tools:$PATH
+export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
 export PATH=$ANDROID_HOME/platform-tools:$PATH
 
 export WSL_HOST=$(tail -1 /etc/resolv.conf | cut -d' ' -f2)
