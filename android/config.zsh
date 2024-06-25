ada78fba004764dc9c88d17a5425856ba34e8ca6
KJ <KJ>
2024-06-25 16:46:37 +0200

    Use correct wsl host

--- android/config.zsh
+++ android/config.zsh
@@ -5,5 +5,5 @@ export ANDROID_SDK_ROOT=$ANDROID_HOME
 export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
 export PATH=$ANDROID_HOME/platform-tools:$PATH
 
-export WSL_HOST=$(tail -1 /etc/resolv.conf | cut -d' ' -f2)
+export WSL_HOST=$(ip route show | grep -i default | awk '{ print $3}')
 export ADB_SERVER_SOCKET=tcp:$WSL_HOST:5037
