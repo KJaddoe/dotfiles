cf6c2e9effaf2cdf8488a17cb7f51d8de2c41be4
KJaddoe <KJaddoe>
2025-05-30 18:19:33 +0200

    Dynamically change NVIM_DIR

--- node/path.zsh
+++ node/path.zsh
@@ -1,4 +1,14 @@
 #!/bin/zsh
-export NVM_DIR=$HOME/.nvm
+
+OS_TYPE="$(uname -s)"
+
+if [[ "$OS_TYPE" == "Darwin" ]]; then
+  if command -v brew >/dev/null 2>&1; then
+    export NVM_DIR="$(brew --prefix nvm)/.nvm"
+  fi
+else
+  export NVM_DIR="$HOME/.nvm"
+fi
+
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
