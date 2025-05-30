e78463e96ae5de4ab4abeac89f17a329b4926792
KJaddoe <KJaddoe>
2025-05-30 16:23:52 +0200

    add missing setup_brew_env function

--- pgFormat/install.sh
+++ pgFormat/install.sh
@@ -28,6 +28,22 @@ verify_installation() {
     echo "pg_format installation completed successfully!"
 }
 
+setup_brew_env() {
+    if [ "$OS" = "Darwin" ]; then
+        if [ -x /opt/homebrew/bin/brew ]; then
+            eval "$(/opt/homebrew/bin/brew shellenv)"  # Apple Silicon
+        elif [ -x /usr/local/bin/brew ]; then
+            eval "$(/usr/local/bin/brew shellenv)"     # Intel
+        fi
+    elif [ "$OS" = "Linux" ]; then
+        if [ -x "$HOME/.linuxbrew/bin/brew" ]; then
+            eval "$($HOME/.linuxbrew/bin/brew shellenv)"
+        elif [ -x /home/linuxbrew/.linuxbrew/bin/brew ]; then
+            eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+        fi
+    fi
+}
+
 setup_brew_env
 case "$OS" in
     Linux)
