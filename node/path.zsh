7c667028041d2f67b06c64f005d4d34cdbe2ae49
KJaddoe <KJaddoe>
2025-05-30 18:30:17 +0200

    Change nvm settings for macos

--- node/path.zsh
+++ node/path.zsh
@@ -4,11 +4,13 @@ OS_TYPE="$(uname -s)"
 
 if [[ "$OS_TYPE" == "Darwin" ]]; then
   if command -v brew >/dev/null 2>&1; then
-    export NVM_DIR="$(brew --prefix nvm)/.nvm"
+    export NVM_DIR="$(brew --prefix nvm)"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/etc/bash_completion" ] && \. "$NVM_DIR/etc/bash_completion"
   fi
 else
   export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 fi
 
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
