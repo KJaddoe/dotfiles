e80928355bd71256be97531552de57af25b6cd8f
KJaddoe <KJaddoe>
2025-05-31 17:06:12 +0200

    Fix error

--- postgres/install.sh
+++ postgres/install.sh
@@ -46,7 +46,6 @@ configure_postgresql() {
 
     if [ "$OS" = "Linux" ]; then
         sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
-    elif [ "$OS" = "Darwin" ]; then
     fi
 
     echo "PostgreSQL setup completed with password 'postgres'."
