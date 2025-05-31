bfa36c044ab4d7b3e3ef2ffe1c60d4ce19bdd32d
KJaddoe <KJaddoe>
2025-05-31 17:37:58 +0200

    Fix wsl settings used on mac

--- android/config.zsh
+++ android/config.zsh
@@ -1,3 +1,9 @@
 #!/bin/zsh
-export WSL_HOST=$(ip route show | grep -i default | awk '{ print $3}')
-export ADB_SERVER_SOCKET=tcp:$WSL_HOST:5037
+
+OS_TYPE="$(uname -s)"
+
+if [[ "$OS" == "Linux" ]]; then
+  export WSL_HOST=$(ip route show | grep -i default | awk '{ print $3}')
+  export ADB_SERVER_SOCKET=tcp:$WSL_HOST:5037
+elif [[ "$OS" == "Darwin" ]]; then
+fi
