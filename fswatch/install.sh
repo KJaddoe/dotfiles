93db60b57f979ad5f834a67df17d0842e641bba5
KJaddoe <KJaddoe>
2025-06-01 02:37:23 +0200

    Fix tmux errir

--- fswatch/install.sh
+++ fswatch/install.sh
@@ -5,8 +5,8 @@ set -e
 OS="$(uname)"
 
 setup_brew_env() {
-    if [ "$OS" = "Darwin" ]; then
-        if [ -x /opt/homebrew/bin/brew ]; then
+    # if [ "$OS" = "Darwin" ]; then
+    #     if [ -x /opt/homebrew/bin/brew ]; then
             eval "$(/opt/homebrew/bin/brew shellenv)"  # Apple Silicon
         elif [ -x /usr/local/bin/brew ]; then
             eval "$(/usr/local/bin/brew shellenv)"     # Intel
