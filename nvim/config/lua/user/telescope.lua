12dc84cbc7e3d696b3ff74d0c1a9f018eda5ec14
KJaddoe <k.jaddoe@xibis.nl>
2026-03-23 15:57:09 +0100

    Move select_one_or_multi to the correct location

--- nvim/config/lua/user/telescope.lua
+++ nvim/config/lua/user/telescope.lua
@@ -1,21 +1,5 @@
 local telescope = require("telescope")
 
-local select_one_or_multi = function(prompt_bufnr)
-  local picker =
-    require("telescope.actions.state").get_current_picker(prompt_bufnr)
-  local multi = picker:get_multi_selection()
-  if not vim.tbl_isempty(multi) then
-    require("telescope.actions").close(prompt_bufnr)
-    for _, j in pairs(multi) do
-      if j.path ~= nil then
-        vim.cmd(string.format("%s %s", "edit", j.path))
-      end
-    end
-  else
-    require("telescope.actions").select_default(prompt_bufnr)
-  end
-end
-
 telescope.load_extension("gh")
 telescope.load_extension("harpoon")
 
