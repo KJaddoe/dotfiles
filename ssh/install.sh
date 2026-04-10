dff06908199acbdae39be6523d90d30940332bd0
KJaddoe <k.a.jaddoe@gmail.com>
2026-04-10 17:00:23 +0200

    Setup ssh using ansible

--- ssh/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p "$HOME/.ssh"
-
-test -L ~/.ssh/config || {
-	mv ~/.ssh/config ~/.ssh/config.local
-	ln -s "$DOTFILES"/ssh/config ~/.ssh/config
-}
-test -f ~/.ssh/config.local || touch ~/.ssh/config.local
