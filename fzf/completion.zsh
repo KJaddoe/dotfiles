68c14fe3566609da6362b8882ca66d23b6cd2567
KJaddoe <k.jaddoe@xibis.nl>
2026-06-10 13:53:10 +0200

    Supercharge fzf with fd backend and previews

--- fzf/completion.zsh
+++ fzf/completion.zsh
@@ -1,2 +1,8 @@
 #!/bin/zsh
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_CTRL_T_OPTS="--preview 'bat --color=always --line-range :500 {} 2>/dev/null || cat {}'"
+export FZF_ALT_C_COMMAND='fd --type d --hidden --follow --exclude .git'
+export FZF_ALT_C_OPTS="--preview 'eza --tree --level=2 --color=always {} 2>/dev/null || ls -la {}'"
 source <(fzf --zsh)
