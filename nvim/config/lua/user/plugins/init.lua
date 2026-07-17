060b2c24655892783816435d0d477c4a1ce72951
KJaddoe <k.jaddoe@xibis.nl>
2026-07-17 18:04:27 +0200

    nvim: migrate better-escape to the new mappings API

--- nvim/config/lua/user/plugins/init.lua
+++ nvim/config/lua/user/plugins/init.lua
@@ -178,8 +178,14 @@ require("lazy").setup({
     "max397574/better-escape.nvim",
     config = function()
       require("better_escape").setup({
-        mapping = { "jk", "kj" },
         timeout = 100,
+        default_mappings = false,
+        mappings = {
+          i = {
+            j = { k = "<Esc>" },
+            k = { j = "<Esc>" },
+          },
+        },
       })
     end,
   },
