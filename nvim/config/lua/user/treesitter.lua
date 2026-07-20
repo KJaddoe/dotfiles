a9a29b1e947e97f6a1a654667d02c06f529359b9
KJaddoe <k.jaddoe@xibis.nl>
2026-07-20 15:35:58 +0200

    nvim: add claudecode.nvim integration on <leader>a
    Move the treesitter param-swap mappings to <leader>sa / <leader>sA to free
    <leader>a for the Claude Code keymaps.

--- nvim/config/lua/user/treesitter.lua
+++ nvim/config/lua/user/treesitter.lua
@@ -63,10 +63,10 @@ require("nvim-treesitter.configs").setup({
     swap = {
       enable = true,
       swap_next = {
-        ["<leader>a"] = "@parameter.inner",
+        ["<leader>sa"] = "@parameter.inner",
       },
       swap_previous = {
-        ["<leader>A"] = "@parameter.inner",
+        ["<leader>sA"] = "@parameter.inner",
       },
     },
     move = {
