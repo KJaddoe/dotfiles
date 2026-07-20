76b5ec9be59daf1e216cef0d7c716b165132ae84
KJaddoe <k.jaddoe@xibis.nl>
2026-07-20 15:36:10 +0200

    nvim: enable Oil filesystem watching and simplify its spec

--- nvim/config/lua/user/plugins/init.lua
+++ nvim/config/lua/user/plugins/init.lua
@@ -494,40 +494,36 @@ require("lazy").setup({
   },
   {
     "stevearc/oil.nvim",
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- Lazy loading is tricky to get right with oil; keep it eager.
+    lazy = false,
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
-    -- Optional dependencies
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-    lazy = false,
-    config = function()
-      require("oil").setup({
-        default_file_explorer = true,
-        skip_confirm_for_simple_edits = true,
-        view_options = {
-          show_hidden = true,
-          natural_order = true,
-          is_always_hidden = function(name, _)
-            return name == ".." or name == ".git"
-          end,
-        },
-        float = {
-          padding = 2,
-          max_width = 100,
-          max_height = 0,
-        },
-        win_options = {
-          wrap = true,
-          winblend = 0,
-        },
-        keymaps = {
-          ["<C-c>"] = false,
-          ["q"] = "actions.close",
-        },
-      })
-    end,
+    opts = {
+      default_file_explorer = true,
+      skip_confirm_for_simple_edits = true,
+      watch_for_changes = true,
+      view_options = {
+        show_hidden = true,
+        natural_order = true,
+        is_always_hidden = function(name, _)
+          return name == ".." or name == ".git"
+        end,
+      },
+      float = {
+        padding = 2,
+        max_width = 100,
+        max_height = 0,
+      },
+      win_options = {
+        wrap = true,
+        winblend = 0,
+      },
+      keymaps = {
+        ["<C-c>"] = false,
+        ["q"] = "actions.close",
+      },
+    },
   },
   {
     "pearofducks/ansible-vim",
