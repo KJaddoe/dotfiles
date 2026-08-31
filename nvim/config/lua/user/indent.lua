fca775278909be98598e3eff1b82339be0efb6ce
KJaddoe <k.jaddoe@xibis.nl>
2026-08-31 17:27:33 +0200

    nvim: remove treesj
    Unused: splitting and joining is left to the formatter on save. Its lazy spec
    also claimed <space>m, a key Harpoon owned, so pressing it loaded treesj for
    nothing.

--- nvim/config/lua/user/indent.lua
+++ nvim/config/lua/user/indent.lua
@@ -3,12 +3,3 @@ require("ibl").setup({
   exclude = { filetypes = { "help" } },
   scope = { enabled = false },
 })
-
-local treesj = require("treesj")
-treesj.setup()
-
-vim.keymap.set("n", "<leader>gS", treesj.toggle, {
-  noremap = true,
-  silent = true,
-  desc = "Toggle split/join",
-})
--- nvim/config/lua/user/plugins/init.lua
+++ nvim/config/lua/user/plugins/init.lua
@@ -430,15 +430,6 @@ require("lazy").setup({
       })
     end,
   },
-  {
-    "Wansmer/treesj",
-    keys = { "<space>m", "<space>j", "<space>s" },
-    dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
-    config = function()
-      require("treesj").setup({ --[[ your config ]]
-      })
-    end,
-  },
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
   { "nvim-treesitter/nvim-treesitter-textobjects" },
   { "nvim-treesitter/nvim-treesitter-context" },
